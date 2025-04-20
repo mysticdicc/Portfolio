ab5ac4113897ed32235d288b3223ac8e8b593baf
richard <ichard@danknet.uk>
2025-04-20 04:31:53 +0100

    small js interop change to stop infinite scrolling issue

--- Portfolio/Portfolio/wwwroot/js/layout.js
+++ Portfolio/Portfolio/wwwroot/js/layout.js
@@ -1,7 +1,21 @@
-﻿function scrollResponse() {
-    var div = document.getElementById('blobber');
+﻿function addEventListener() {
+    document.getElementById('layout').addEventListener('scroll', scrollResponse);
+}
+
+function scrollResponse() {
+    var blobber = document.getElementById('blobber');
     var layout = document.getElementById('layout');
+    var body = document.getElementById('body');
+
+    var window = window;
 
     var layoutY = layout.scrollTop;
-    div.style.top = layoutY - 50  + 'px';
+    var blobberTop = blobber.style.top;
+    var blobberTopInt = parseFloat(blobberTop);
+    var pos = blobber.clientHeight + blobberTopInt;
+
+    if (body.clientHeight >= pos || blobberTop == '')
+    {
+        blobber.style.top = layoutY + 10 + 'px';
+    }
 }
